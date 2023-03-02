9eda3c4412ed49af8f0484ed8f3a89c63c33df24
ramazan <ramazan.aksoy1907@hotmail.com>
2023-03-02 12:41:12 +0300

    pdf viewer  network add

--- lib/feature/home/service/Ihome_service.dart
+++
@@ -1,7 +0,0 @@
-import '/feature/home/model/user.dart';
-
-import '../../../core/base/model/IResponseModel.dart';
-
-abstract class IHomeService {
-  Future<IResponseModel<List<Post>?>> getPosts();
-}
--- lib/feature/home/view/home_screen.dart
+++ lib/feature/home/view/home_screen.dart
@@ -1,39 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import '/core/init/theme/theme_service.dart';
-import 'package:provider/provider.dart';
-import '../model/user.dart';
-import '../viewModel/home_view_model.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    HomeViewModel homeViewModel = HomeViewModel();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Example"),
-      ),
-      body: Observer(builder: (_) {
-        return homeViewModel.isLoading
-            ? ListView.builder(
-                itemCount: homeViewModel.post?.length ?? 0,
-                itemBuilder: (context, index) {
-                  List<Post>? list = homeViewModel.post;
-                  return ListTile(
-                    subtitle: Text(list?[index].body ?? ''),
-                    title: Text(list?[index].title ?? ''),
-                  );
-                },
-              )
-            : const Center(child: CircularProgressIndicator());
-      }),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            context.read<ThemeService>().changeTheme();
-          },
-          child: const Icon(Icons.change_circle_outlined)),
-    );
+        body: Container(
+            child: SfPdfViewer.network(
+      'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+    )));
   }
 }
